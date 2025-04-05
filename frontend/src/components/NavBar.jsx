58fba7a0997a9e5c5fe524b3f98f8221fc8f799c
Mayank Gupta <mayank.gupta8@stryker.com>
2025-04-05 18:29:39 +0530

    sticky removed from the navabr

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -105,7 +105,7 @@ const NavBar = () => {
   };
 
   return (
-    <nav className="bg-blue-900 shadow-sm border-b border-gray-800 sticky top-0 z-50">
+    <nav className="bg-blue-900 shadow-sm border-b border-gray-800 top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center">
           {/* Logo and App Name Container */}

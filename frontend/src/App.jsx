18f3297b927289ae599149a2b7461d9ae8a0a146
Mayank Gupta <mayank.gupta8@stryker.com>
2025-04-05 13:43:39 +0530

    routes added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Navigate, Routes, Route } from "react-router-dom";
-import { useEffect } from "react";
 import { useAuthStore } from "./store/useAuthStore";
 import { Loader } from "lucide-react";
 import { Toaster } from "react-hot-toast";
+
+// Components
 import NavBar from "./components/NavBar";
+
+// Pages
 import LoginPage from "./pages/LoginPage";
-import CreateBlogPage from "./pages/CreateBlogPage";
+import SignupPage from "./pages/SignupPage";
 import ForgotPasswordPage from "./pages/ForgotPasswordPage";
 import ResetPasswordPage from "./pages/ResetPasswordPage";
-import SignupPage from "./pages/SignupPage";
+import CreateBlogPage from "./pages/CreateBlogPage";
 import UserDashboard from "./pages/UserDashboard";
 import PublicBlogsPage from "./pages/PublicBlogsPage";
+import CreateBlogWithAi from "./pages/CreateBlogWithAi";
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
@@ -28,52 +32,56 @@ const App = () => {
     );
 
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <NavBar />
 
-      <Routes>
-        {/* Public blogs page as the main landing page */}
-        <Route path="/" element={<PublicBlogsPage />} />
+      <main className="flex-grow">
+        <Routes>
+          {/* Public routes accessible to everyone */}
+          <Route path="/" element={<PublicBlogsPage />} />
 
-        {/* Auth routes - redirect to dashboard if already logged in */}
-        <Route
-          path="/login"
-          element={!authUser ? <LoginPage /> : <Navigate to="/dashboard" />}
-        />
-        <Route
-          path="/signup"
-          element={!authUser ? <SignupPage /> : <Navigate to="/dashboard" />}
-        />
-        <Route
-          path="/forgot-password"
-          element={
-            !authUser ? <ForgotPasswordPage /> : <Navigate to="/login" />
-          }
-        />
-        <Route
-          path="/reset-password/:resetToken"
-          element={!authUser ? <ResetPasswordPage /> : <Navigate to="/login" />}
-        />
+          {/* Authentication routes - redirect to dashboard if already logged in */}
+          <Route
+            path="/login"
+            element={!authUser ? <LoginPage /> : <Navigate to="/dashboard" />}
+          />
+          <Route
+            path="/signup"
+            element={!authUser ? <SignupPage /> : <Navigate to="/dashboard" />}
+          />
+          <Route
+            path="/forgot-password"
+            element={
+              !authUser ? <ForgotPasswordPage /> : <Navigate to="/dashboard" />
+            }
+          />
+          <Route
+            path="/reset-password/:resetToken"
+            element={
+              !authUser ? <ResetPasswordPage /> : <Navigate to="/dashboard" />
+            }
+          />
 
-        {/* Protected routes - require authentication */}
-        <Route
-          path="/dashboard"
-          element={authUser ? <UserDashboard /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/create-blog"
-          element={authUser ? <CreateBlogPage /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/edit-blog/:blogId"
-          element={authUser ? <CreateBlogPage /> : <Navigate to="/login" />}
-        />
+          {/* Protected routes - require authentication */}
+          <Route
+            path="/dashboard"
+            element={authUser ? <UserDashboard /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/create-blog"
+            element={authUser ? <CreateBlogPage /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/edit-blog/:blogId"
+            element={authUser ? <CreateBlogPage /> : <Navigate to="/login" />}
+          />
 
-        {/* Fallback to public blogs for unknown routes */}
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+          {/* Fallback route */}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </main>
 
-      <Toaster />
+      <Toaster position="top-center" reverseOrder={false} />
     </div>
   );
 };
---
+++ frontend/src/pages/CreateBlogWithAi.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreateBlogWithAi = () => {
+  return <div>CreateBlogWithAi</div>;
+};
+
+export default CreateBlogWithAi;
